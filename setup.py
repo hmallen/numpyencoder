c20f30c1efb35ddd316c67c0e87a663f62d15859
hmallen <allenhm@gmail.com>
2018-05-29 19:39:58 -0500

    Adding files to repo.

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setup(
+    name='numpyencoder',
+    version='0.1.0',
+    author='Hunter M. Allen',
+    author_email='allenhm@gmail.com',
+    license='MIT',
+    #packages=find_packages(),
+    packages=['numpyencoder'],
+    install_requires=['numpy>=1.14.3'],
+    description='Python JSON encoder for handling Numpy data types.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/hmallen/numpyencoder',
+    keywords=['numpy', 'json', 'encoder'],
+    classifiers=(
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ),
+)
