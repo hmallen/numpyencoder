d9d3eb3c5b7158d96267b71267ce2c443c7766c1
Hunter Allen <allenhm@gmail.com>
2025-05-01 05:28:02 -0500

    Bump to 0.3.0: Add NumPy 2.x compatibility

--- setup.py
+++ setup.py
@@ -1,15 +1,14 @@
 from setuptools import setup
 
-with open("README.md", "r") as fh:
+with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
     name="numpyencoder",
-    version="0.2.0",
+    version="0.3.0",
     author="Hunter M. Allen",
     author_email="allenhm@gmail.com",
     license="MIT",
-    # packages=find_packages(),
     packages=["numpyencoder"],
     install_requires=["numpy>=1.14.3"],
     description="Python JSON encoder for handling Numpy data types.",
@@ -17,9 +16,10 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/hmallen/numpyencoder",
     keywords=["numpy", "json", "encoder"],
-    classifiers=(
+    classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-    ),
+    ],
+    python_requires=">=3.6",
 )
