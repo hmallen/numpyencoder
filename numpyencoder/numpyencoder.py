894a170567a3bb4b1168feece82ae90728c44c05
Neil Shephard <n.shephard@sheffield.ac.uk>
2025-05-15 14:14:40 +0100

    bug: remove deprectaed np.complex_ type

--- numpyencoder/numpyencoder.py
+++ numpyencoder/numpyencoder.py
@@ -28,7 +28,7 @@ class NumpyEncoder(json.JSONEncoder):
         elif isinstance(obj, (np.float16, np.float32, np.float64)):
             return float(obj)
 
-        elif isinstance(obj, (np.complex_, np.complex64, np.complex128)):
+        elif isinstance(obj, (np.complex64, np.complex128)):
             return {"real": obj.real, "imag": obj.imag}
 
         elif isinstance(obj, (np.ndarray,)):
