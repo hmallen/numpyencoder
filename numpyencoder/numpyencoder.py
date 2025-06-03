de0af4a64616daf103762e8491591ff10875d75b
Neil Shephard <n.shephard@sheffield.ac.uk>
2025-06-03 15:00:21 +0100

    feature: handle Numpy type deprecation & backwards compatibility
    Closes #7
    
    Detects Numpy version on the fly and sets the types for `float` and `complex` conditional on whether `np.__version__ < "2.0.0"`.
    
    Includes a warning advising users to consider updating them if using an old version of Numpy.
    
    No idea if this is a sensible way of approaching such issues so happy to be advised/guided on better ways of handling such issues.

--- numpyencoder/numpyencoder.py
+++ numpyencoder/numpyencoder.py
@@ -1,5 +1,26 @@
+import warnings
 import json
 import numpy as np
+
+
+# Both np.float_ and np.complex_ were deprecated in Numpy 2.0.0
+if np.__version__ < "2.0.0":
+    TYPES = {
+        "float": (np.float_, np.float16, np.float32, np.float64),
+        "complex": (np.complex_, np.complex64, np.complex128),
+    }
+    warnings.warn(
+        f"You are using an old version of Numpy ({np.__version__}), "
+        "support for 'np.float_' and 'np.complex_' were deprecated "
+        "in Numpy 2.0.0. If you use these datatypes in your code "
+        "please consider updating them.",
+        category=DeprecationWarning,
+    )
+else:
+    TYPES = {
+        "float": (np.float16, np.float32, np.float64),
+        "complex": (np.complex64, np.complex128),
+    }
 
 
 class NumpyEncoder(json.JSONEncoder):
@@ -22,13 +43,12 @@ class NumpyEncoder(json.JSONEncoder):
                 np.uint64,
             ),
         ):
-
             return int(obj)
 
-        elif isinstance(obj, (np.float16, np.float32, np.float64)):
+        elif isinstance(obj, TYPES["float"]):
             return float(obj)
 
-        elif isinstance(obj, (np.complex_, np.complex64, np.complex128)):
+        elif isinstance(obj, TYPES["complex"]):
             return {"real": obj.real, "imag": obj.imag}
 
         elif isinstance(obj, (np.ndarray,)):
